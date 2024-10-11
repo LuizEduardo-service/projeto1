2437c0ea536fe03b99ec3c087b833dddf2df9883
LuizEduardo-service <luizeduardo.service@outlook.com>
2024-10-11 07:42:05 -0300

    feat: criando urls

--- projeto_django1/urls.py
+++ projeto_django1/urls.py
@@ -15,14 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http.response import HttpResponse
-
-def my_view(request):
-    
-    return HttpResponse('hello word')
+from django.urls import path, include
+from recipes.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sobre/', my_view),
+    path('', include('recipes.urls')),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'recipes/home.html')
+
+def contato(request):
+    return HttpResponse('Tela contato')
+
+def sobre(request):
+    return HttpResponse('Tela sobre')
