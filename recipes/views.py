87fd7392c3481917bc58b924c748e59464195c77
LuizEduardo-service <luizeduardo.service@outlook.com>
2024-10-23 08:04:04 -0300

    feat: ajustando urls

--- recipes/views.py
+++ recipes/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 from utils.recipes.factory import make_recipe
 
+
 def home(request):
     mock_recipes = [make_recipe() for _ in range(10)]
     return render(request, 'recipes/pages/home.html', context={"recipes": mock_recipes})
 
 def recipe(request, id: int):
     mock_recipe = make_recipe()
-    return render(request, 'recipes/pages/recipe-view.html', context={"recipe": mock_recipe})
+    return render(request, 'recipes/pages/recipe-view.html',
+                  context={"recipe": mock_recipe, 'is_detail_page': True}
+                  )
 
