6b40c425eba8f4eabb34a06f82269291c83a9210
LuizEduardo-service <luizeduardo.service@outlook.com>
2024-10-28 07:25:40 -0300

    feat: ajustando category

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 from utils.recipes.factory import make_recipe
-
+from recipes.models import Recipe
 
 def home(request):
-    mock_recipes = [make_recipe() for _ in range(10)]
-    return render(request, 'recipes/pages/home.html', context={"recipes": mock_recipes})
+    # mock_recipes = [make_recipe() for _ in range(10)]
+    recipes = Recipe.objects.filter(is_published= True).all().order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={"recipes": recipes})
+
+def category(request, category_id: int):
+    recipe_category = Recipe.objects.filter(category__id=category_id, is_published = True)
+    return render(request, 'recipes/pages/category.html', context={"recipes": recipe_category})
 
 def recipe(request, id: int):
-    mock_recipe = make_recipe()
+    # mock_recipe = make_recipe()
+    recipe = Recipe.objects.filter(id=id).first()
     return render(request, 'recipes/pages/recipe-view.html',
-                  context={"recipe": mock_recipe, 'is_detail_page': True}
+                  context={"recipe": recipe, 'is_detail_page': True}
                   )
 
