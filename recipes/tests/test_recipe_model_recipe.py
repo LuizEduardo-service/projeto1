09a72a751b4c9df56d5a2127ed97ccd80f93f984
LuizEduardo-service <luizeduardo.service@outlook.com>
2024-11-14 08:12:47 -0300

    feat: iniciando test models

---
+++ recipes/tests/test_recipe_model_recipe.py
@@ -0,0 +1,23 @@
+from . test_recipe_base import TestRecipeBase
+from django.core.exceptions import ValidationError
+
+from parameterized import parameterized
+class RecipeModelTest(TestRecipeBase):
+
+    def setUp(self) -> None:
+        self.recipe = self.make_recipe()
+        return super().setUp()
+
+    @parameterized.expand([
+            ('title', 65),
+            ('description', 165),
+            ('preparation_time_unit', 65),
+            ('servings_unit', 65),
+        ])
+    def test_recipe_fields_max_lenght(self, field, max_length):
+
+
+
+        setattr(self.recipe, field, 'a' * (max_length + 1)) # metodo setattr: seta os dados dentro do atributo do objeto
+        with self.assertRaises(ValidationError):
+            self.recipe.full_clean()
