b3ce0067ef413814a563d1d9ac11a832db17934d
LuizEduardo-service <luizeduardo.service@outlook.com>
2024-12-09 07:46:42 -0300

    feat: criando app authors

--- projeto_django1/urls.py
+++ projeto_django1/urls.py
@@ -6,6 +6,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls')),
+    path('authors/', include('authors.urls')),
 ]
 
 # habilitando os arquivos estaticos
