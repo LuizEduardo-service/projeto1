db7e820f8fbbad5434b1a51390fdd3d9013f75d4
LuizEduardo-service <luizeduardo.service@outlook.com>
2024-12-12 08:12:56 -0300

    feat: incluindo formulario

--- authors/urls.py
+++ authors/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'authors'
 urlpatterns = [
     path('register/', views.register_view, name="register"),
+    path('register/create/', views.register_create, name="create"),
 ]
