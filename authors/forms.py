5d99c38c7a9f0b7629185dfac3c43fc876d1e635
LuizEduardo-service <luizeduardo.service@outlook.com>
2024-12-13 08:06:06 -0300

    feat: formatando campos

--- authors/forms.py
+++ authors/forms.py
@@ -6,3 +6,27 @@ class RegisterForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username','email', 'password']
+        labels = {
+            'username': 'Nome de usuário',
+            'first_name': 'Nome',
+            'last_name': 'Sobrenome',
+            'email': 'Email',
+            'password': 'Senha',
+        }  
+        help_texts={
+            'email': 'Digite um email válido'
+        }   
+
+        error_messages = {
+            'username':{
+                'required': 'Esse campo não deve ser vazio',
+            }
+        }  
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'placeholder': 'insira seu nome de usuario'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'insira sua senha aqui'
+            })
+        }
