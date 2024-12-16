bbd3913a4ef8f67a69dc3c609b8a859d4d9c439f
LuizEduardo-service <luizeduardo.service@outlook.com>
2024-12-16 08:08:57 -0300

    feat: criando validações de dados

--- authors/forms.py
+++ authors/forms.py
@@ -1,8 +1,37 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+import re
+
+def add_attr(field, attr_name, attr_new_val):
+    existing_attr = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing_attr} {attr_new_val}'.strip()
+
+def add_placeholder(field, placeholder_val):
+    field.widget.attrs['placeholder'] = placeholder_val
 
 
+def strong_password(password):
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+    if not regex.match(password):
+        raise ValidationError('Pass deve conter letras maiusculas, minusculas e numeros',code='invalid')
+    
 class RegisterForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields['username'], 'Digite seu nome')
+        add_placeholder(self.fields['email'], 'Digite seu email')
+
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Digite a senha novamente'
+            }
+        ),
+        validators=[strong_password]
+    )
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username','email', 'password']
@@ -30,3 +59,13 @@ class RegisterForm(forms.ModelForm):
                 'placeholder': 'insira sua senha aqui'
             })
         }
+
+    def clean(self):
+        cleaned_data =  super().clean()
+        password = cleaned_data.get('password')
+        password2 = cleaned_data.get('password2')
+
+        if password != password2:
+            raise ValidationError({
+                'password': 'Senhas não conferem'
+            })
